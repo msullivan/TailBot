342859ee0f82c82c0e23395ba4b752ec92869ae8
Michael Sullivan <sully@msully.net>
2013-02-25 22:35:25 -0500

    Modify the bot for my particular purposes.

--- FollowTail.py
+++ FollowTail.py
@@ -1,27 +1,54 @@
 from twisted.internet.task import LoopingCall
-from os import stat, fstat, path
+from os import stat, fstat, path, listdir
+import codecs
 
 class FollowTail:
     fileObj = None
-    
-    def __init__(self, filename, callback, *a, **kw):
-        self.filename = filename
-        assert path.exists(filename)
+
+    def __init__(self, directory, base, callback, *a, **kw):
+        self.encoding = 'utf-16'
+        self.special_delim = ' ] '
+
+        self.directory = directory
+        self.base = base
+        self.filename = self.find_file()
+        print "Found file %s for %s" % (self.filename, base)
+        assert path.exists(self.file_path())
         self.lc = LoopingCall(self.check)
         self.callback = callback
         self.a, self.kw = a, kw
-        
+
     def start(self, checkFreq = 0.5):
-        self.fileObj = open(self.filename)
+        self.fileObj = codecs.open(self.file_path(), 'rb',
+                                   encoding=self.encoding)
         self.fileObj.seek(0, 2)
         self.lc.start(checkFreq)
-        
+
+    def file_path(self):
+        return path.join(self.directory, self.filename)
+
+    def find_file(self):
+        return max(f for f in listdir(self.directory) if self.base in f)
+
     def check(self):
         obj, callback, a, kw = self.fileObj, self.callback, self.a, self.kw
         obj.seek(obj.tell())
         for line in obj:
             line = line.strip()
-            callback(line, self.filename, *a, **kw)
+            if self.special_delim not in line: continue
+            line = line.split(self.special_delim, 1)[1]
+
+            line = line.encode('utf-8')
+            callback(line, self.base, *a, **kw)
+
+        newfile = self.find_file()
+        if newfile != self.filename:
+            self.filename = newfile
+            print "Switching to %s for %s" % (newfile, self.base)
+            self.fileObj.close()
+            self.fileObj = codecs.open(self.file_path(), 'rb',
+                                       encoding=self.encoding)
+
 
     def stop(self):
         if self.lc.running:
@@ -33,14 +60,14 @@ class FollowTail:
 class ChainCallback:
     def __init__(self, *callbacks):
         self.callbacks = list(callbacks)
-        
+
     def __call__(self, *a, **kw):
         for callback in self.callbacks:
             callback(*a, **kw)
-            
+
     def addCallback(self, callback):
         self.callbacks.append(callback)
-        
+
     def removeCallback(self, callback):
         if callback in self.callbacks:
             self.callbacks.remove(callback)
--- Run.py
+++ Run.py
@@ -4,7 +4,7 @@ from twisted.internet import reactor
 from twisted.python import log
 log.startLogging(sys.stdout)
 if __name__ == "__main__":
-    
+
     try:
         configFile = sys.argv[1]
     except:
@@ -16,31 +16,32 @@ if __name__ == "__main__":
     except:
         print "Config File Not Found! [%s]" % configFile
         os.exit()
-        
+
     try:
         server = config.get('ircd', 'server')
         port = config.getint('ircd', 'port')
         ssl = config.getboolean('ircd', 'ssl')
         channels = [c.strip() for c in config.get('ircd', 'channel').split(',')]
         nickname = config.get('ircd', 'nickname')
+        directory = config.get('files', 'directory')
         files = [f.strip() for f in config.get('files', 'filenames').split(';')]
     except e:
         print "Invalid Configuration Directives! Run ConfigMaker.py [%s]" % e
         os.exit()
-    
+
     print "[TailBot] starting with config file [%s]" % configFile
     print "Connecting to %s:%i [ssl=%s] as %s" % (server, port, ssl, nickname)
     print "Joining: %s" % (', '.join(channels))
     print "Tailing: %s" % (', '.join(files))
-    
+
     factory = TailBot.TailBotFactory(channels, nickname)
     for file in files:
-        factory.addTailFollower(file)
-        
+        factory.addTailFollower(directory, file)
+
     if ssl:
         from twisted.internet import ssl
         reactor.connectSSL(server, port, factory, ssl.ClientContextFactory())
     else:
         reactor.connectTCP(server, port, factory)
-        
+
     reactor.run()
